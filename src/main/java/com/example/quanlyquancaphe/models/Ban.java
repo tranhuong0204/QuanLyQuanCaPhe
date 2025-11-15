448c6ec69def6f2d35b9caab26988fcbaaa30024
ngdhiep <hiepga24681@gmail.com>
2025-11-16 00:58:42 +0700

    BanClass

---
+++ src/main/java/com/example/quanlyquancaphe/models/Ban.java
@@ -0,0 +1,62 @@
+package com.example.quanlyquancaphe.models;
+
+public class Ban {
+    private String maBan;      // Mã định danh của bàn
+    private String viTri;      // Khu vực: Tầng 1, Tầng 2, Sảnh, VIP...
+    private int soGhe;         // Số lượng ghế
+    private String trangThai;  // Ví dụ: "Trống", "Đang dùng", "Đặt trước"
+    private String ghiChu;     // Ghi chú thêm
+
+    public Ban(){
+
+    }
+
+    public Ban(String maBan, String viTri, int soGhe, String trangThai, String ghiChu) {
+        this.maBan = maBan;
+        this.viTri = viTri;
+        this.soGhe = soGhe;
+        this.trangThai = trangThai;
+        this.ghiChu = ghiChu;
+    }
+
+    public String getMaBan() {
+        return maBan;
+    }
+
+    public void setMaBan(String maBan) {
+        this.maBan = maBan;
+    }
+
+    public String getViTri() {
+        return viTri;
+    }
+
+    public void setViTri(String viTri) {
+        this.viTri = viTri;
+    }
+
+    public int getSoGhe() {
+        return soGhe;
+    }
+
+    public void setSoGhe(int soGhe) {
+        this.soGhe = soGhe;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+}
+
