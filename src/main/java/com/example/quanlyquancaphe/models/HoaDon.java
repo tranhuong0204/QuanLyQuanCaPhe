b9daf4f25d081207f0b68b3654bdc60888090a35
ntngochuyen <huyen2005dz@gmail.com>
2025-12-13 16:28:01 +0700

    Update HoaDon.java

--- src/main/java/com/example/quanlyquancaphe/models/HoaDon.java
+++ src/main/java/com/example/quanlyquancaphe/models/HoaDon.java
@@ -1,26 +1,27 @@
 package com.example.quanlyquancaphe.models;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class HoaDon {
     private String maHoaDon;
     private int tongKM;
-    private int tongTien;
+    private BigDecimal tongTien;   // sửa từ int -> BigDecimal
     private Date ngayLap;
-    private String phuongThucThanhToan;
+    //private String phuongThucThanhToan;
     private String maBan;
     private String maTaiKhoan;
-    private String maPT; // phương thức thanh toán
+    private String maPT;
 
     public HoaDon() {}
 
-    public HoaDon(String maHoaDon, int tongKM, int tongTien, Date ngayLap,
-                  String phuongThucThanhToan, String maBan, String maTaiKhoan, String maPT) {
+    public HoaDon(String maHoaDon, int tongKM, BigDecimal tongTien, Date ngayLap/*,
+                  String phuongThucThanhToan*/, String maBan, String maTaiKhoan, String maPT) {
         this.maHoaDon = maHoaDon;
         this.tongKM = tongKM;
         this.tongTien = tongTien;
         this.ngayLap = ngayLap;
-        this.phuongThucThanhToan = phuongThucThanhToan;
+        //this.phuongThucThanhToan = phuongThucThanhToan;
         this.maBan = maBan;
         this.maTaiKhoan = maTaiKhoan;
         this.maPT = maPT;
@@ -31,12 +32,12 @@ public class HoaDon {
     public void setMaHoaDon(String maHoaDon) { this.maHoaDon = maHoaDon; }
     public int getTongKM() { return tongKM; }
     public void setTongKM(int tongKM) { this.tongKM = tongKM; }
-    public int getTongTien() { return tongTien; }
-    public void setTongTien(int tongTien) { this.tongTien = tongTien; }
+    public BigDecimal getTongTien() { return tongTien; }
+    public void setTongTien(BigDecimal tongTien) { this.tongTien = tongTien; }
     public Date getNgayLap() { return ngayLap; }
     public void setNgayLap(Date ngayLap) { this.ngayLap = ngayLap; }
-    public String getPhuongThucThanhToan() { return phuongThucThanhToan; }
-    public void setPhuongThucThanhToan(String phuongThucThanhToan) { this.phuongThucThanhToan = phuongThucThanhToan; }
+    //public String getPhuongThucThanhToan() { return phuongThucThanhToan; }
+    //public void setPhuongThucThanhToan(String phuongThucThanhToan) { this.phuongThucThanhToan = phuongThucThanhToan; }
     public String getMaBan() { return maBan; }
     public void setMaBan(String maBan) { this.maBan = maBan; }
     public String getMaTaiKhoan() { return maTaiKhoan; }
